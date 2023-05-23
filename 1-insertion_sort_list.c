d3a0c92dc21126c5a52f1d46043eb9518bcb03f2
xiayudev <jctuesta7@gmail.com>
2023-05-23 15:33:12 -0700

    feat: sort linked list with insertion algorithm

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,47 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - function to sort a linked list in ascending order
+ * @list: The linked list
+ *
+ * Using insertion sort algorithm to sort a doubly linked list
+ *
+ * Return: Always void
+ *
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *head = *list, *temp, *aux;
+
+	if (!(*list))
+		return;
+	while (head)
+	{
+		temp = head;
+		while (temp->prev && temp->n < temp->prev->n)
+		{
+			aux = temp;
+			temp = temp->prev;
+			temp->next = aux->next;
+			if (aux->next)
+				aux->next->prev = temp;
+			aux->next = temp;
+			aux->prev = temp->prev;
+			if (temp->prev)
+			{
+				temp->prev->next = aux;
+				temp->prev = aux;
+			}
+			else
+				temp->prev = aux;
+			while (aux->prev)
+				aux = aux->prev;
+			print_list(aux);
+			temp = temp->prev;
+		}
+		head = head->next;
+	}
+	while (temp->prev)
+		temp = temp->prev;
+	(*list) = temp;
+}
